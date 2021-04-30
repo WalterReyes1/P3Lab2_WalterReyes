7dc42f02763ef9272211ada67c01e5dc60797100
walter reyes <walter.reyes@cambridgehn.com>
2021-04-30 16:39:48 -0600

    Listo!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <math.h>
 #include <cmath>
+#include<stdlib.h>
+#include<time.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 void pascal(int,int,int[],int[]);
 void sumatoria();
 void grafico();
-int opcion = 0;
+void imprimir(int [], int tam);
 
+int opcion = 0;
 int menu() {
 	while(true) {
 		cout<<"Bienvenido "<<endl;
@@ -28,19 +31,19 @@ int main(int argc, char** argv) {
 		switch(opcion=menu()) {
 			case 1: {
 				int max_n =0;
-				do{
-				cout<<"Ingrese un valor del 1 al 99: ";
-				cin>>max_n;	
-				}while(max_n<1 || max_n>99);
-					int salida[100];
-					int res_anterior[100];
-					
-					for(int i = 0; i <100;i++){
-						salida[i]=0;
-						res_anterior[i]=0;
-					}
-					pascal(0,max_n,res_anterior,salida);
-					
+				do {
+					cout<<"Ingrese un valor del 1 al 99: ";
+					cin>>max_n;
+				} while(max_n<1 || max_n>99);
+				int salida[100];
+				int res_anterior[100];
+
+				for(int i = 0; i <100; i++) {
+					salida[i]=0;
+					res_anterior[i]=0;
+				}
+				pascal(0,max_n, res_anterior, salida);
+
 				break;
 			}
 			case 2: {
@@ -48,7 +51,7 @@ int main(int argc, char** argv) {
 				break;
 			}
 			case 3: {
-				//grafico();
+				grafico();
 				break;
 			}
 			case 4: {
@@ -62,61 +65,109 @@ int main(int argc, char** argv) {
 	return 0;
 
 }
-void pascal(int n, int max_n,int res_anterior [100], int salida [100] ){
-	
-	int x = n+1;
-	if(n==max_n){
-		
-	}else{
-		if(n==0){
-			salida[n]=1;
-			res_anterior[n]=1;	
-			salida[x]=1;		
-			cout<<salida[n]<<endl;
-			pascal(n+1,max_n,res_anterior,salida);
-		}else{
-			
-			for(int i = x; i<=n;i++){
-				salida[i]=1;
-				
-				salida[i]=res_anterior[i-1]+res_anterior[i];
-			}
-			
-			
-			cout<<endl;
-			
-		}
-		
-	pascal(n+1,max_n,res_anterior,salida);	
-	}
-}// fin pascal;
 
-void sumatoria(){
-	int arreglo[]= {6,2,3,1};
+
+
+void sumatoria() {
+	int arreglo[20];
+	
+	for(int i = 0; i <20; i++) {
+		arreglo[i]=1+rand()%99;
+	}
+	imprimir(arreglo,20);
+	cout<<endl;
 	double acum=0.0;
 	double div = 0.0;
 	double positivo =0.0;
 	double acum2=0.0;
-	for(int i=0; i<4;i++){
+	for(int i=0; i<20; i++) {
 		acum+= arreglo[i];
-		
+
 	}
-	div= acum/4;
-	for(int i=0;i<4;i++){
-	positivo=arreglo[i]-div;
-		if(positivo <0){
+	div= acum/20;
+	for(int i=0; i<20; i++) {
+		positivo=arreglo[i]-div;
+		if(positivo <0) {
 			(positivo)*-1.0;
-			
 		}
-		
-		
+
+
 		acum2+= pow(positivo,2);
-		
+
 	}
-	double total = sqrt(acum2/4.0);
-	
+	double total = sqrt(acum2/20.0);
+
 	cout<<"El resultado es: "<<total<<endl;
-	
-	
-	
+
 }
+
+void grafico() {
+	int arreglo[10];
+	int arreglo2[10];
+	srand (time(NULL));
+	for(int i = 0; i <10; i++) {
+
+		arreglo[i]= 1+rand()% 19;
+		arreglo2[i]= 1+rand()% 19;
+	}
+	imprimir(arreglo,10);
+	imprimir(arreglo2,10);
+	char x = 177;
+	char y = 178;
+	cout<<endl;
+	for(int i = 0; i<10; i++) {
+		cout<<endl;
+		cout<<i+1<<". ";
+		for(int j = 0; j<arreglo[i]; j++) {
+			cout<<x<<" ";
+		}
+		cout<<endl;
+		cout<<"   ";
+		for(int j = 0; j< arreglo2[i]; j++) {
+			cout<<y<<" ";
+		}
+		cout<<endl;
+	}
+
+
+}
+void imprimir(int arreglo [],int tam) {
+	cout<<"[ ";
+	for(int i = 0; i<tam; i++) {
+		cout<<arreglo[i]<<", ";
+	}
+	cout<<"] "<<endl;
+}
+
+void pascal(int n, int max_n,int res_anterior[],int salida[] ) {
+	int final= 0;
+	if(n==max_n) {
+		cout<<endl;
+		cout<<"Al fin lo pude hacer :D \n"<<endl;
+		
+	} else {
+		if(n==final) {
+			salida[n]=1;
+			res_anterior[n]=1;
+			cout<<salida[n]<<endl;
+			pascal(n+1,max_n,res_anterior,salida);
+		} else {
+			for(int i = 0; i< n; i++) {
+
+				salida[i]=res_anterior[i-1]+res_anterior[i];
+
+				cout<<salida[i]<<" ";
+			}
+
+			for(int i = 0; i<n; i++) {
+				res_anterior[i]=salida[i];
+				salida[i]=final;
+			}
+			cout<<endl;
+			pascal(n+1,max_n,res_anterior,salida);
+		}
+
+	}
+}// fin pascal;
+
+
